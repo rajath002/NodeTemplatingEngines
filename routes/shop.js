ab17cf2ad15d5a9ef91446de5643ef8be0996014
rajathkumar <rajath.kumar@bureauofdata.com>
2019-10-10 11:21:37 +0530

    init

---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.log("Admin data products : ", adminData.products);
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+module.exports = router;
