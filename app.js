6d19b19d8c5987eb4b0cb6c619f2572871ef3fa7
RajatKD <acharya.drajath@gmail.com>
2019-10-10 23:50:01 +0530

    ejs engine updated

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 
 const app = express();
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set("view engine", "hbs");
+
+// Setting view engine to EJS
+app.set('view engine', 'ejs');
+
+// Setting view engine to Handlebars
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.set("view engine", "hbs");
 
 // Setting view-engine to pug.
 // app.set("view engine", "pug");
@@ -25,7 +30,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page NOT FOUND'});
 });
 
 app.listen(3000);
