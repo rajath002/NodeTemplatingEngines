235e6681af4efc314b4f341622614bfa86ad532f
RajatKD <acharya.drajath@gmail.com>
2019-10-10 23:16:47 +0530

    handlebars Completed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const expressHbs = require('express-handlebars');
 
 
 const app = express();
-app.engine('hbs', expressHbs());
+app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 app.set("view engine", "hbs");
 
 // Setting view-engine to pug.
